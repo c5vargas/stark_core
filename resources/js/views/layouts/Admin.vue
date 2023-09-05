41f1e1c2d1218c7af56720ca2e5fc9842a76781e
Carles Vargas <carlesvargas5@gmail.com>
2023-09-05 18:41:30 +0200

    feat: AdminNotices component improvements

--- resources/js/views/layouts/Admin.vue
+++ resources/js/views/layouts/Admin.vue
@@ -6,6 +6,7 @@
             <admin-navbar />
 
             <div class="page-content">
+                <admin-notices :notice="notices[0]" />
                 <slot></slot>
             </div>
 
@@ -16,7 +17,18 @@
 </template>
 
 <script setup>
+import {ref,onBeforeMount} from 'vue'
 import AdminSidebar from './parts/AdminSidebar.vue';
 import AdminNavbar from './parts/AdminNavbar.vue';
 import AdminFooter from './parts/AdminFooter.vue'
+import AdminNotices from '@/views/components/Shared/AdminNotices.vue';
+import {useSettingsStore} from '@/stores/settings'
+
+const store = useSettingsStore()
+const notices = ref([])
+
+onBeforeMount( async() => {
+    await store.fetch();
+    notices.value = store.getNotices();
+})
 </script>
