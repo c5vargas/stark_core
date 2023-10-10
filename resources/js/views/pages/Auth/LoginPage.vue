2c75e47d64b98378792977af71dc4c2c02d937b7
c5vargas <carlesvargas5@gmail.com>
2023-10-10 08:28:53 +0200

    style: added background on LoginPage

--- resources/js/views/pages/Auth/LoginPage.vue
+++ resources/js/views/pages/Auth/LoginPage.vue
@@ -7,9 +7,6 @@
                     <div class="col-md-8 col-xl-6 mx-auto">
                         <div class="card">
                             <div class="row">
-                                <div class="col-md-4 pe-md-0">
-                                    <div class="auth-side-wrapper" style="background-image: url('https://picsum.photos/219/452')"></div>
-                                </div>
                                 <div class="col-md-8 ps-md-0">
                                     <div class="auth-form-wrapper px-4 py-5">
                                         <a href="#" class="noble-ui-logo d-block mb-2">Noble<span>UI</span></a>
