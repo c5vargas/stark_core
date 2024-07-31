ca4736c86bab681e125d41b2dc57f1c56804ea33
c5vargas <carlesvargas5@gmail.com>
2024-07-31 09:26:18 +0200

    styles: improvements on gdpr form component

--- resources/js/contexts/admin/settings/gdpr/components/FormComponent.vue
+++ resources/js/contexts/admin/settings/gdpr/components/FormComponent.vue
@@ -7,10 +7,29 @@
       <p class="mb-0">{{ $t('dashboard.settings.gdpr_long_desc') }}</p>
     </div>
   </div>
+
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link border-0 active" id="cookies-tab" data-bs-toggle="tab" data-bs-target="#cookies" type="button" role="tab" aria-controls="cookies" aria-selected="true">
+        {{ $t('dashboard.settings.gdpr.gdpr_cookies_page') }}
+      </button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link border-0" id="privacy-tab" data-bs-toggle="tab" data-bs-target="#privacy" type="button" role="tab" aria-controls="privacy" aria-selected="false">
+        {{ $t('dashboard.settings.gdpr.gdpr_privacy_page') }}
+      </button>
+    </li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane fade show active" id="cookies" role="tabpanel" aria-labelledby="cookies-tab">
+      <CookiesFormComponent />
+    </div>
+    <div class="tab-pane fade" id="privacy" role="tabpanel" aria-labelledby="privacy-tab">
+      <PrivacyFormComponent />  
+    </div>
+  </div>
   
-  <PrivacyFormComponent />
   
-  <CookiesFormComponent />
 </template>
 
 <script setup>
