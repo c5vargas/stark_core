073fd480dcb04faaeba31e692ac997871cc09971
Carles Vargas <carlesvargas5@gmail.com>
2025-09-10 08:35:16 +0200

    feat: removed unused google properties

--- resources/js/contexts/settings/components/GoogleAnalyticsForm.tsx
+++ resources/js/contexts/settings/components/GoogleAnalyticsForm.tsx
@@ -7,7 +7,6 @@ import { useTranslation } from 'react-i18next'
 import { FormField } from '@/contexts/shared/components/ui/form/FormField'
 import { InputText } from '@/contexts/shared/components/ui/form/InputText'
 import { BaseButton } from '@/contexts/shared/components/Button'
-import { InputFile } from '@/contexts/shared/components/ui/form/InputFile'
 import { HelpSquareIcon } from '@/contexts/shared/components/HugeIcons'
 
 export const GoogleAnalyticsForm: React.FC = () => {
@@ -18,16 +17,12 @@ export const GoogleAnalyticsForm: React.FC = () => {
   const [form, setForm] = useState<Partial<SettingsMap>>({
     analytics_property_id: '',
     manager_measurement_id: '',
-    maps_api_key: '',
   })
-
-  // const [file, setFile] = useState<File | null>(null) TODO: Cargar fichero al sistema
 
   useEffect(() => {
     setForm({
       analytics_property_id: settings.analytics_property_id || '',
       manager_measurement_id: settings.manager_measurement_id || '',
-      maps_api_key: settings.maps_api_key || '',
     })
   }, [settings])
 
@@ -46,7 +41,6 @@ export const GoogleAnalyticsForm: React.FC = () => {
 
   return (
     <div className="space-y-4">
-      {/* Info Card */}
       <InfoCard
         title={t('dashboard.settings.google.analytics')}
         description={t('dashboard.settings.google.analytics_desc')}
@@ -70,22 +64,8 @@ export const GoogleAnalyticsForm: React.FC = () => {
         </a>
       </InfoCard>
 
-      {/* Form Card */}
       <InfoCard>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <FormField label={t('dashboard.settings.google.account_key')}>
-            <InputFile name="account_key_file" onChange={handleChange} accept=".json,.p12" />
-          </FormField>
-
-          <FormField label={t('dashboard.settings.google.analytics_property')}>
-            <InputText
-              type="text"
-              name="analytics_property_id"
-              value={form.analytics_property_id}
-              onChange={handleChange}
-            />
-          </FormField>
-
           <FormField
             label={t('dashboard.settings.google.manager_measurement')}
             helperText={t('dashboard.settings.google.manager_measurement_desc')}
@@ -95,18 +75,6 @@ export const GoogleAnalyticsForm: React.FC = () => {
               name="manager_measurement_id"
               placeholder="G-******"
               value={form.manager_measurement_id}
-              onChange={handleChange}
-            />
-          </FormField>
-
-          <FormField
-            label={t('dashboard.settings.google.maps_api_key')}
-            helperText={t('dashboard.settings.google.maps_api_key_desc')}
-          >
-            <InputText
-              type="text"
-              name="maps_api_key"
-              value={form.maps_api_key}
               onChange={handleChange}
             />
           </FormField>
